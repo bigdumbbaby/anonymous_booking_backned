51370ad3071e4bdc8663946d8d90660a7fcc29c3
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-18 15:34:11 -0600

    added cors and express.json()

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-port = process.env.PORT || 80
+const cors = require('cors')
+port = process.env.PORT || 3000
 
 // const knex = require('knex')
 // const config = require('./knexfile')[process.env.NODE_ENV || 'development']
@@ -9,6 +10,9 @@ port = process.env.PORT || 80
 const ownerRouter = require('./routes/owners')
 const loginRouter = require('./routes/login')
 const secretRouter = require('./routes/secret-route')
+
+app.use(cors())
+app.use(express.json())
 
 app.use('/owners', ownerRouter)
 app.use('/login', loginRouter)
