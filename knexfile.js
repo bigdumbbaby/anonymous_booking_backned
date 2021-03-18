8e24c50f6d4516f20054d135fbb6709d764ef6c6
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-18 15:26:48 -0600

    set up procfile for heroku

--- knexfile.js
+++ knexfile.js
@@ -6,4 +6,13 @@ module.exports = {
     client: 'pg',
     connection: 'postgresql://localhost/ANONYMOUS_BOOKING_DB'
   },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/migrations'
+    }
+  }
+
 };
