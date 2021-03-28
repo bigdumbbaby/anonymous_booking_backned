93873439f5d8cb0c8ed419c84e169f03c5604bae
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-28 17:41:41 -0600

    added route to connections

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const loginRouter = require('./routes/login')
 const secretRouter = require('./routes/secret-route')
 const artistRouter = require('./routes/artists')
 const venueRouter = require('./routes/venues')
+const connectionRouter = require('./routes/connections')
 
 app.use(cors())
 app.use(express.json())
@@ -21,6 +22,7 @@ app.use('/login', loginRouter)
 app.use('/secretRoute', secretRouter)
 app.use('/artists', artistRouter)
 app.use('/venues', venueRouter)
+app.use('/connections', connectionRouter)
 
 app.listen(port, () => {
   console.log(`LISTENING ON PORT ${port}`)
