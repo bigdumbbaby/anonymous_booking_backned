1e7c0cf78267832a9e67395849d4335206b57245
bigdumbbaby <colton.shawn.oconnor@gmail.com>
2021-03-30 15:09:22 -0600

    fixed venue spelling

--- routes/connections.js
+++ routes/connections.js
@@ -81,7 +81,7 @@ router.post('/getMyConnections', (request, response) => {
           .then(artist => {
             return {
               ...connection, 
-              artist[0]
+              artist
             }
           })
       }))
